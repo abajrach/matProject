79cd853a51efa395521dddbc5f813e4e9f30a02f
Arbindra B <arbindrabajracharya@gmail.com>
2018-11-10 22:58:37 -0600

    added about and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/material.module';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule', },
+  { path: 'about', component: AboutComponent, },
   { path: '**', redirectTo:'dashboard' }
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
